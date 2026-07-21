b0992c03562fceaa0a79a5692d685ad19784e685
mlengse <anjangkn@gmail.com>
2026-07-21 19:28:41 +0700

    chore: integrate eslint into project and configure linting scripts

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "script",
+      globals: {
+        module: "readonly",
+        require: "readonly",
+        exports: "writable",
+        console: "readonly",
+        Among: "readonly",
+        SnowballProgram: "readonly",
+      },
+    },
+    rules: {
+      eqeqeq: "warn",
+      "no-unused-vars": ["warn", { args: "none", varsIgnorePattern: "^(Among|SnowballProgram|\\w+Stemmer)$" }],
+      "no-undef": "error",
+      "no-redeclare": "off",
+      "no-unused-labels": "warn",
+      "no-fallthrough": "warn",
+    },
+  },
+  {
+    ignores: ["stemmer/lib/**", "node_modules/**"],
+  },
+];
